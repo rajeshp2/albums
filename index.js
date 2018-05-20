20c397680a612e2d0deb4f3e786d45aedb1ec489
rajeshpanchaneni <p2rajesh111@gmail.com>
2018-05-20 08:31:22 +0530

    moved header.js to components folder

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+// Index.js - place code in here for Android!!!
+
+// Import a library to help create a component
+import React from 'react';
 import { AppRegistry } from 'react-native';
 import App from './App';
+import Header from './src/components/header';
 
+// Create a component
+const App = () => (
+    <Header />
+  );
+
+// Render it to the device
 AppRegistry.registerComponent('albums', () => App);
