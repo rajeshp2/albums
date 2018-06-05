51de66c93a960b3a23e41bfd4b9d8f3ab5442a61
rajeshpanchaneni <p2rajesh111@gmail.com>
2018-06-05 17:28:40 +0530

    Creating List

---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const AlbumDetail = () => {
+  return (
+     <View>
+       <Text>{props.album.title}</Text>
+     </View>
+  );
+};
+export default AlbumDetail;
