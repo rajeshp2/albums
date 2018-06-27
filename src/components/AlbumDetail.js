39636136ad6c1a022b8082e1b22db0a49152aea6
rajeshpanchaneni <p2rajesh111@gmail.com>
2018-06-27 19:04:41 +0530

    Button Creation

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
   const { title, artist, thumbnail_image, image } = album;
@@ -33,6 +34,9 @@ const AlbumDetail = ({ album }) => {
            source={{ uri: image }}
         />
       </CardSection>
+      <CardSection>
+        <Button />
+      </CardSection>
     </Card>
   );
 };
@@ -40,8 +44,8 @@ const AlbumDetail = ({ album }) => {
 const styles = {
   headerContentStyle: {
     flexDirection: 'column',
-    justifyContent: 'space-around'
-  },
+    justifyContent: 'space-around',
+},
   headerTextStyle: {
     fontSize: 18
   },
---
+++ src/components/Button.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+
+
+const Button = () => {
+  const { buttonStyle, textStyle } = styles;
+   return (
+     <TouchableOpacity style={buttonStyle}>
+        <Text style={textStyle}>Donate or Receive
+        </Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = {
+  textStyle: {
+    alignSelf: 'center',
+    color: '#17161b',
+    fontSize: 16,
+    fontWeight: '600',
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  buttonStyle: {
+    flex: 1,
+    alignSelf: 'stretch',
+    backgroundColor: '#ddd',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#ff0000',
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 5
+  }
+};
+
+export default Button;
