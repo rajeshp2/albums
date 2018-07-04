856c555a8992bc53850fd46b0045668aae4bf23a
rajeshpanchaneni <p2rajesh111@gmail.com>
2018-07-04 22:39:06 +0530

    Revert "change"
    This reverts commit aa3f2bab6521e1bd3b4c99c36657a505bc17e0f9.

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,8 @@ import { View } from 'react-native';
 const Card = (props) => {
  return (
     <View style={styles.containerStyle}>
-      {props.children}</View>
+      {props.children}
+    </View>
    );
 };
 
